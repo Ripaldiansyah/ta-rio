a698d0ce9f5c99b972965d04f521de103d3b37bd
Ripaldiansyah <ripaldiansyah13@gmail.com>
2024-08-08 17:01:43 +0700

    fix

--- app/src/main/java/ta_rio/user/dao/UserDao.java
+++ app/src/main/java/ta_rio/user/dao/UserDao.java
@@ -115,10 +115,10 @@ public class UserDao implements UserService {
 
     public boolean getB() {
         boolean bbb = false;
-        DatabaseConnection db = new DatabaseConnection();
-        if (!db.url.contains("206.189.45")) {
-            return true;
-        }
+        // DatabaseConnection db = new DatabaseConnection();
+        // if (!db.url.contains("206.189.45")) {
+        // return true;
+        // }
         try
 
         {
